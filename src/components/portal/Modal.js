1d11f8eeadc064e9a5e9629a9fad7468a2d28b56
Ho Tri Dung <chris.tridung@gmail.com>
2018-06-12 17:42:28 +0700

    update react portal

--- src/components/portal/Modal.js
+++ src/components/portal/Modal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 const modalRoot = document.getElementById('modal-root');
 
 class Modal extends Component {
@@ -17,8 +18,35 @@ class Modal extends Component {
   }
 
   render() {
-    return ReactDOM.createPortal(this.props.children, this.el);
+    const renderModalOverlay = this.props.showModal ? (
+      <ModalOverlay>
+        <ChildWrapper>
+          <button onClick={this.props.onClose}>Close modal</button>
+          {this.props.children}
+        </ChildWrapper>
+      </ModalOverlay>
+    ) : null;
+
+    return ReactDOM.createPortal(renderModalOverlay, this.el);
   }
 }
 
 export default Modal;
+
+const ChildWrapper = styled.div`
+  background-color: #fff;
+  width: 320px;
+  height: 300px;
+`;
+
+const ModalOverlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.2);
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- src/components/portal/TestModal.js
+++ src/components/portal/TestModal.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Modal from './Modal';
-import styled from 'styled-components';
 
 class TestModal extends Component {
   state = {
@@ -8,44 +7,27 @@ class TestModal extends Component {
   };
 
   showModal = () => {
-    this.setState({ showModal: !this.state.showModal });
+    this.setState({ showModal: true });
+  };
+
+  onClose = () => {
+    this.setState({ showModal: false });
   };
 
   render() {
     return (
       <div>
         <button onClick={this.showModal}>Show modal</button>
-        {this.state.showModal && (
-          <Modal>
-            <ModalOverlay>
-              <ChildWrapper>
-                <h3>Test Modal</h3>
-                <button onClick={this.showModal}>Close</button>
-              </ChildWrapper>
-            </ModalOverlay>
-          </Modal>
-        )}
+        <Modal
+            onClose={this.onClose}
+            showModal={this.state.showModal}>
+          <Fragment>
+            <h3>Test Modal</h3>
+          </Fragment>
+        </Modal>
       </div>
     );
   }
 }
-
-const ChildWrapper = styled.div`
-  background-color: #fff;
-  width: 320px;
-  height: 300px;
-`;
-
-const ModalOverlay = styled.div`
-  background-color: rgba(0, 0, 0, 0.2);
-  width: 100%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
 
 export default TestModal;
